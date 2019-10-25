dbf6056f42a51e368be6b8012918caafe89b9338
Kristina <55129343+lalkakris@users.noreply.github.com>
2019-10-25 11:07:34 +0300

    Update Program.cs

--- DndMonsters/DndMonsters/Program.cs
+++ DndMonsters/DndMonsters/Program.cs
@@ -28,8 +28,20 @@ namespace DndMonsters
             }
             
             //Max armor class:
-            var MaxArmor = monsters.OrderBy(x => x.ArmorClass).Max(x => x.ArmorClass);
-            Console.WriteLine($"\nMonster with the max armor class is {MaxArmor}");
+            var MaxArmor = monsters.OrderBy(x => x.ArmorClass).Max(x => (x.ArmorClass, x.Name));
+            Console.WriteLine($"\n\nMonster with the max armor class is {MaxArmor}");
+
+            //Monsters with claws:
+            var Claws = monsters.Where(x => x.Weapon.Name == "Claws");
+            Console.WriteLine("\nThere are list of monsters with claws:");
+            foreach (var monster in Claws)
+            {
+                Console.Write($"{ monster.Name} ");
+            }
+
+            //First monster with 2<level<4
+            var monster1 = monsters.FirstOrDefault(x => x.Level > 2 && x.Level < 4);
+            Console.WriteLine($"\n\nThe first monster with level more then 2 and less then 4 is {monster1.Name}");
         }
     }
 }
